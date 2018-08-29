db87b7d004774a09c2ae9989d0956e66ec20947a
peterstaranchuk <petr.staranchuk@gmail.com>
2018-08-29 15:01:59 +0300

    Add additional logic to resize animation class (to resize horizontally as well as vertically)

--- app/src/main/java/com/storiqa/storiqawallet/object/ResizeAnimation.kt
+++ app/src/main/java/com/storiqa/storiqawallet/object/ResizeAnimation.kt
@@ -5,11 +5,17 @@ import android.view.animation.Animation
 import android.view.animation.Transformation
 
 
-class ResizeAnimation(internal var view: View, private val targetHeight: Int, private var startHeight: Int) : Animation() {
+class ResizeAnimation(internal var view: View,
+                      private val targetHeight: Int,
+                      private val startHeight: Int,
+                      private val targetWidth: Int,
+                      private val startWidth: Int) : Animation() {
 
     override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
         val newHeight = (startHeight+(targetHeight - startHeight) * interpolatedTime).toInt()
+        val newWidth = (startWidth + (targetWidth - startWidth) * interpolatedTime).toInt()
         view.layoutParams.height = newHeight
+        view.layoutParams.width = newWidth
         view.requestLayout()
     }
 
