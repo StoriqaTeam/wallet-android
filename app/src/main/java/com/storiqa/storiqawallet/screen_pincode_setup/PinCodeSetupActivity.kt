ad20f042699bffad7c9f1ecd7486570348933b48
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-04 12:44:36 +0300

    Add set pin code setup or skip activity

---
+++ app/src/main/java/com/storiqa/storiqawallet/screen_pincode_setup/PinCodeSetupActivity.kt
@@ -0,0 +1,15 @@
+package com.storiqa.storiqawallet.screen_pincode_setup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.storiqa.storiqawallet.R
+
+class PinCodeSetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin_code_setup)
+
+
+    }
+}
