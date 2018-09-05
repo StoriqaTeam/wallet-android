7efac5bdfc76e03ef567aedbb8cc9d89fc4aa562
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-05 14:26:28 +0300

    Add quick launch screens in ScreenStarter Add layouts for blue and white round buttons

--- app/src/main/java/com/storiqa/storiqawallet/objects/ScreenStarter.kt
+++ app/src/main/java/com/storiqa/storiqawallet/objects/ScreenStarter.kt
@@ -3,10 +3,15 @@ package com.storiqa.storiqawallet.objects
 import android.content.Context
 import android.content.Intent
 import com.storiqa.storiqawallet.constants.Extras
+import com.storiqa.storiqawallet.enums.PinCodeEnterType
 import com.storiqa.storiqawallet.screen_login.LoginActivity
+import com.storiqa.storiqawallet.screen_pin_code_enter.EnterPinCodeActivity
+import com.storiqa.storiqawallet.screen_pincode_setup.PinCodeSetupActivity
+import com.storiqa.storiqawallet.screen_quick_start.QuickStartActivity
 import com.storiqa.storiqawallet.screen_recover_password.screen_recover_email_enter_first_step.RecoverPasswordActivity
 import com.storiqa.storiqawallet.screen_recover_password.screen_recover_password_enter_second_step.NewPasswordEnterActivity
 import com.storiqa.storiqawallet.screen_register.RegisterActivity
+import com.storiqa.storiqawallet.screen_setup_fingerprint.FingerprintSetupActivity
 
 class ScreenStarter {
 
@@ -17,4 +22,14 @@ class ScreenStarter {
     fun startRecoverPasswordScreen(context: Context) = context.startActivity(Intent(context, RecoverPasswordActivity::class.java))
 
     fun startNewPasswordEnterScreen(context: Context, email: String) = context.startActivity(Intent(context, NewPasswordEnterActivity::class.java).putExtra(Extras().email, email))
+
+    fun startPinCodeSetupScreen(context: Context) = context.startActivity(Intent(context, PinCodeSetupActivity::class.java).putExtra(Extras().pinCodeEnterType, PinCodeEnterType.ENTER_PASSWORD_FIRST_TIME))
+
+    fun startEnterPinCodeScreen(context: Context) = context.startActivity(Intent(context, EnterPinCodeActivity::class.java))
+
+    fun startQuickStartScreen(context: Context) = context.startActivity(Intent(context, QuickStartActivity::class.java))
+
+    fun startFingerprintSetupScreen(context: Context) = context.startActivity(Intent(context, FingerprintSetupActivity::class.java))
+
+    fun startMainScreen(context: Context) {} //TODO add screen
 }
