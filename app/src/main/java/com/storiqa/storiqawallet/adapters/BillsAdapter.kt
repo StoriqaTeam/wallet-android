f3bd989924b60f461683ca04f4bb2c33cca7fb95
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-17 19:54:46 +0300

    Rename item_card to item_bill

--- app/src/main/java/com/storiqa/storiqawallet/adapters/BillsAdapter.kt
+++ app/src/main/java/com/storiqa/storiqawallet/adapters/BillsAdapter.kt
@@ -1,11 +1,14 @@
 package com.storiqa.storiqawallet.adapters
 
 import android.content.Context
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.storiqa.storiqawallet.databinding.ItemCardBinding
+import com.storiqa.storiqawallet.databinding.ItemBillBinding
 import com.storiqa.storiqawallet.objects.Bill
+import com.storiqa.storiqawallet.objects.BillInfo
+import kotlinx.android.synthetic.main.item_bill.view.*
 import org.jetbrains.anko.dip
 
 class BillsAdapter(private val bills : Array<Bill>, val onClick : (position : Int) -> Unit) : RecyclerView.Adapter<BillsAdapter.ViewHolder>() {
@@ -17,7 +20,7 @@ class BillsAdapter(private val bills : Array<Bill>, val onClick : (position : In
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
-        = ViewHolder(ItemCardBinding.inflate(getInflater(parent.context), parent, false))
+        = ViewHolder(ItemBillBinding.inflate(getInflater(parent.context), parent, false))
 
     override fun getItemCount(): Int = bills.size
 
@@ -25,13 +28,25 @@ class BillsAdapter(private val bills : Array<Bill>, val onClick : (position : In
         holder.bind(bills[position], onClick)
     }
 
-    class ViewHolder(private val binding : ItemCardBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private val binding : ItemBillBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(bill: Bill, onClick : (position : Int) -> Unit) {
-            binding.bill = bill
-            binding.executePendingBindings()
-            binding.root.setOnClickListener { _ -> onClick(layoutPosition) }
-            binding.root.setPadding(0,0,0, binding.root.context.dip(17))
-        }
 
+            binding.apply {
+                this.bill = bill
+                executePendingBindings()
+                root.setOnClickListener { _ -> onClick(layoutPosition) }
+                root.setPadding(0,0,0, root.context.dip(17))
+
+                val billInfo = BillInfo(bill)
+                root.tvBillStatus.text = root.context.getString(billInfo.getBillStatus())
+                root.ivBillImage.setBackgroundResource(billInfo.getBillImage())
+                root.clBill.setBackgroundResource(billInfo.getBillColor())
+
+                val textColor = ResourcesCompat.getColor(root.context.resources, billInfo.getBillTextColor(), null)
+                root.tvTokenType.setTextColor(textColor)
+                root.tvHolderName.setTextColor(textColor)
+                root.tvAmount.setTextColor(textColor)
+            }
+        }
     }
 }
