325f8852b4e97ef3d55fd494d755a92aaeb397bb
DmitryKruglov <d.kruglov@storiqa.com>
2018-12-13 10:35:53 +0300

    commit

--- app/src/main/java/com/storiqa/storiqawallet/network/common/RequestHeaders.kt
+++
@@ -1,7 +0,0 @@
-package com.storiqa.storiqawallet.network.common
-
-data class RequestHeaders(
-        val timestamp: String,
-        val deviceId: String,
-        val sign: String,
-        val bearer: String? = null)
