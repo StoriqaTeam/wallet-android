21382a66c946c6ec747eb0f73b9d6dfb375ab64a
Tata <t.grigorevskaya@storiqa.com>
2019-01-23 11:24:40 +0300

    Settings layout

--- app/src/main/java/com/storiqa/storiqawallet/ui/main/menu/MenuFragment.kt
+++ app/src/main/java/com/storiqa/storiqawallet/ui/main/menu/MenuFragment.kt
@@ -1,16 +1,13 @@
 package com.storiqa.storiqawallet.ui.main.menu
 
+import android.os.Bundle
+import androidx.preference.PreferenceFragment
+import androidx.preference.PreferenceFragmentCompat
 import com.storiqa.storiqawallet.BR
 import com.storiqa.storiqawallet.R
-import com.storiqa.storiqawallet.databinding.FragmentPasswordResetBinding
-import com.storiqa.storiqawallet.ui.base.BaseFragment
 
-class MenuFragment : BaseFragment<FragmentPasswordResetBinding, MenuViewModel>() {
-
-    override fun getLayoutId(): Int = R.layout.fragment_menu
-
-    override fun getBindingVariable(): Int = BR.viewModel
-
-    override fun getViewModelClass(): Class<MenuViewModel> = MenuViewModel::class.java
-
+class MenuFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
 }
