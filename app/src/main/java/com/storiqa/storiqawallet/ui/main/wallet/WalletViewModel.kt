1a20da300b2b3485612c6a15b92764c95974a0fa
Dmitry Kruglov <d.kruglov@storiqa.com>
2019-02-08 17:12:41 +0300

    fix design update vibration

--- app/src/main/java/com/storiqa/storiqawallet/ui/main/wallet/WalletViewModel.kt
+++ app/src/main/java/com/storiqa/storiqawallet/ui/main/wallet/WalletViewModel.kt
@@ -7,7 +7,6 @@ import com.storiqa.storiqawallet.common.SingleLiveEvent
 import com.storiqa.storiqawallet.data.IAppDataStorage
 import com.storiqa.storiqawallet.data.ITokenProvider
 import com.storiqa.storiqawallet.data.IUserDataStorage
-import com.storiqa.storiqawallet.data.db.AppDatabase
 import com.storiqa.storiqawallet.data.db.entity.AccountEntity
 import com.storiqa.storiqawallet.data.db.entity.RateEntity
 import com.storiqa.storiqawallet.data.mapper.AccountMapper
@@ -15,26 +14,17 @@ import com.storiqa.storiqawallet.data.model.Account
 import com.storiqa.storiqawallet.data.polling.ShortPolling
 import com.storiqa.storiqawallet.data.repository.IAccountsRepository
 import com.storiqa.storiqawallet.data.repository.IRatesRepository
-import com.storiqa.storiqawallet.data.repository.ITransactionsRepository
-import com.storiqa.storiqawallet.data.repository.IUserRepository
-import com.storiqa.storiqawallet.network.WalletApi
 import com.storiqa.storiqawallet.ui.base.BaseViewModel
 import com.storiqa.storiqawallet.ui.main.IMainNavigator
 import com.storiqa.storiqawallet.ui.main.account.AccountFragment
-import com.storiqa.storiqawallet.utils.SignUtil
 import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
 class WalletViewModel
 @Inject
 constructor(navigator: IMainNavigator,
-            private val walletApi: WalletApi,
-            private val signUtil: SignUtil,
-            private val appDatabase: AppDatabase,
-            private val userRepository: IUserRepository,
             private val accountsRepository: IAccountsRepository,
             private val ratesRepository: IRatesRepository,
-            private val transactionsRepository: ITransactionsRepository,
             private val userData: IUserDataStorage,
             private val appData: IAppDataStorage,
             private val tokenProvider: ITokenProvider) : BaseViewModel<IMainNavigator>() {
@@ -52,11 +42,8 @@ constructor(navigator: IMainNavigator,
         val token = appData.token
         if (tokenProvider.isExpired(token))
             tokenProvider.refreshToken({
-                //appData.token = it
-                updateData()
             }, ::handleError)
 
-        val signHeader = signUtil.createSignHeader(appData.currentUserEmail)
 
         ratesRepository.getRates()
                 .observeOn(AndroidSchedulers.mainThread())
@@ -75,12 +62,6 @@ constructor(navigator: IMainNavigator,
 
         ShortPolling(accountsRepository, ratesRepository).start(userData.id, userData.email)
 
-        transactionsRepository.refreshTransactions(userData.id, appData.currentUserEmail, 0).subscribe({
-
-        }, {
-            print("error")
-        })
-
     }
 
     private fun updateAccounts() {
@@ -90,12 +71,6 @@ constructor(navigator: IMainNavigator,
             accounts.forEach { cards.add(mapper.map(it)) }
             updateAccounts.value = cards
         }
-    }
-
-    private fun updateData() {
-        //userRepository.refreshUser(::handleError)
-
-        //accountsRepository.refreshAccounts(::handleError)
     }
 
     fun onAccountClicked(position: Int, element: View, transition: String) {
